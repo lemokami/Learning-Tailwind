ed862bd8cb377f51e2d292d6798cb6fd213bc059
jesswinw <jesswinw@gmail.com>
2020-08-07 00:31:52 +0530

    new primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: [],
   theme: {
     extend: {
+      colors: {
+        primary: "#e63946",
+      },
       fontFamily: {
         body: ["Open Sans", " sans-serif"],
       },
